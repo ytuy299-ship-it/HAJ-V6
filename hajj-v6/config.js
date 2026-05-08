5c4ae776fbb071cd04ad322e86a2abbf71485f9a
ABEER <ytuy299@gmail.com>
2026-05-08 17:21:24 +0300

    تعديل ألوان القائمة

--- hajj-v6/config.js
+++
@@ -1,4 +0,0 @@
-// ضع بيانات Supabase هنا
-// من Supabase: Project Settings > API
-window.SUPABASE_URL = "https://icdirxyrnmkzairxulbc.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljZGlyeHlybm1remFpcnh1bGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDY3MzgsImV4cCI6MjA5MzQ4MjczOH0.AM7Nl70jHydHzedAy0B8wxeDpfqKK4p6rmz9Vzo1XvA";
